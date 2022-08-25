e846707a66fc678f425c077eef4518b4ef34d120
Thắng <thangpaisen@gmail.com>
2022-08-25 19:27:44 +0700

    commit

--- app/components/home/Home.tsx
+++ app/components/home/Home.tsx
@@ -1,15 +1,18 @@
 import { FlatList, StyleSheet, Text, View, Image } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import Constant from './controller/Constant'
 import HeaderNormal from '../common/HeaderNormal'
 import CommonAPIs from './controller/APIs/CommonAPIs'
-
+import FastImage from 'react-native-fast-image'
+import AutoHeightImage from 'react-native-auto-height-image'
+import FitImage from 'react-native-fit-image'
 type Props = {}
 
 const Home = (props: Props) => {
     const [listCharacters, setListCharacters] = useState<any[]>([])
     const [listMangaAnime, setListMangaAnime] = useState<any[]>([])
     const [listPopular, setListPopular] = useState<any[]>([])
+    const [data, setData] = useState<any[]>([])
 
     useEffect(() => {
         CommonAPIs.getAllPopular()
@@ -24,13 +27,39 @@ const Home = (props: Props) => {
             .finally(() => {})
     }, [])
 
+    useEffect(() => {
+        CommonAPIs.getListWallpaperBySlug('aggretsuko-wallpapers')
+            .then((res) => {
+                setData(res)
+            })
+            .catch((err) => {
+                console.log('err', err)
+            })
+            .finally(() => {})
+    }, [])
+
     return (
         <View style={styles.container}>
             <HeaderNormal />
             <FlatList
-                data={listPopular}
+                data={[1, 2, 3, 4, 5, 6, 7, 78, 8, 89, 9]}
                 renderItem={({ item }) => (
-                    <Image source={{ uri: item?.thumbnail }} style={styles.imageItem} />
+                    // <FastImage
+                    //     source={{ uri: item }}
+                    //     style={{
+                    //         ...styles.imageItem
+                    //     }}
+                    //     resizeMode='contain'
+                    // />
+                    <AutoHeightImage
+                        width={(Constant.screen.width - 60) / 2}
+                        source={{ uri: 'https://i.waifu.pics/Tj6Wzwo.png' }}
+                        style={{
+                            marginRight: 20,
+                            marginTop: 20,
+                            borderRadius: 5
+                        }}
+                    />
                 )}
                 numColumns={2}
             />
@@ -51,7 +80,8 @@ const styles = StyleSheet.create({
         marginRight: 20,
         marginTop: 20,
         width: (Constant.screen.width - 60) / 2,
-        height: (Constant.screen.width - 60) / 2,
-        borderRadius: 5
+        aspectRatio: 0.5,
+        borderRadius: 5,
+        resizeMode: 'contain'
     }
 })
