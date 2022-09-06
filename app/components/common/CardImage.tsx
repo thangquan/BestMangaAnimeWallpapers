3308608d30f8eb5d74c6bdb4b5352ae9582a4ebd
Thắng <thangpaisen@gmail.com>
2022-09-06 20:56:53 +0700

    add

--- app/components/common/CardImage.tsx
+++ app/components/common/CardImage.tsx
@@ -3,49 +3,28 @@ import React, { useState, useEffect } from 'react'
 import Constant from '../home/controller/Constant'
 import AutoHeightImage from 'react-native-auto-height-image'
 import FastImage from 'react-native-fast-image'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
 type Props = {
     uri: string
     width?: number
 }
 
-const CardImage = ({ uri = '', width = (Constant.screen.width - 60) / 2 }: Props) => {
-    const [sizeImage, setSizeImage] = useState<any>({
-        width: width,
-        height: 0
-    })
+const CardImage = ({ uri = '', width = (wp(100) - 60) / 2 }: Props) => {
+
     const [loading, setLoading] = useState<boolean>(true)
-
-    const getSizeImage = (uri: string): void => {
-        console.log('1')
-        Image.getSize(uri, (width, height) => {
-            setSizeImage({
-                width: width,
-                height: height
-            })
-            console.log('2')
-            setLoading(false)
-        })
-        console.log('3')
-    }
-    useEffect(() => {
-        getSizeImage(uri)
-    }, [])
-
-    if (loading) {
-        return <ActivityIndicator size='small' color='#0000ff' />
-    }
-
-    return <FastImage source={{ uri }} style={{ ...styles.imageItem, width, ...sizeImage }} />
+    
+    return <FastImage source={{ uri }} style={styles.imageItem} />
 }
 
 export default React.memo(CardImage)
 
 const styles = StyleSheet.create({
     imageItem: {
-        marginRight: 20,
-        marginTop: 20,
-        width: (Constant.screen.width - 60) / 2,
-        borderRadius: 5
+        marginRight: 5,
+        marginTop: 5,
+        width: (wp(100) - 30) / 3,
+        height: wp(45),
+        borderRadius: 2
     }
 })
