51825af77a6ddcec49882882cf694d6716a7e13a
Thắng <thangpaisen@gmail.com>
2022-09-07 22:01:41 +0700

    update ui DrawerContent

--- app/components/common/HeaderNormal.tsx
+++ app/components/common/HeaderNormal.tsx
@@ -1,15 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
 import Icon from 'react-native-vector-icons/Ionicons'
 import Constant from '../../controller/Constant'
+import { useNavigation } from '@react-navigation/native'
 
 type Props = {}
 
 const HeaderNormal = (props: Props) => {
+    const navigation = useNavigation()
+
     return (
         <View style={styles.header}>
             <Text style={styles.txtHeader}>Waifu Pictures</Text>
-            <Icon name='options' size={20} color={'#fff'} />
+            <TouchableOpacity
+                onPress={() => {
+                    navigation.openDrawer()
+                }}
+            >
+                <Icon name='options' size={24} color={'#fff'} />
+            </TouchableOpacity>
         </View>
     )
 }
@@ -25,7 +34,7 @@ const styles = StyleSheet.create({
         marginTop: 20
     },
     txtHeader: {
-        fontSize: 20,
+        fontSize: 22,
         fontFamily: Constant.fonts.robotoSlabSemiBold,
         color: Constant.color.text
     }
--- app/components/drawerContent/DrawerContent.tsx
+++ app/components/drawerContent/DrawerContent.tsx
@@ -1,13 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native'
 import React from 'react'
 import Constant from './../../controller/Constant'
 
 type Props = {}
 
 const DrawerContent = (props: Props) => {
+    const renderItem = ({ item, index }: { item: string; index: number }) => {
+        return (
+            <TouchableOpacity style={styles.itemCategory}>
+                <Text style={styles.textTitleCategory}>{item}</Text>
+            </TouchableOpacity>
+        )
+    }
     return (
         <View style={styles.drawerContent}>
             <Text style={styles.title}>Waifu Pictures</Text>
+            <FlatList
+                data={Constant.categories}
+                keyExtractor={(item) => item}
+                renderItem={renderItem}
+                showsVerticalScrollIndicator={false}
+            />
         </View>
     )
 }
@@ -16,13 +29,28 @@ export default DrawerContent
 
 const styles = StyleSheet.create({
     drawerContent: {
-        flex: 1
+        flex: 1,
+        backgroundColor: Constant.color.backgroundColor
     },
     title: {
-        marginTop: 20,
+        marginVertical: 20,
         fontSize: 26,
         fontFamily: Constant.fonts.robotoSlabSemiBold,
-        color: Constant.color.backgroundColor,
+        color: Constant.color.text,
         textAlign: 'center'
+    },
+    itemCategory: {
+        borderWidth: 0.33,
+        borderColor: '#3F4E4F',
+        marginHorizontal: 20,
+        marginBottom: 10,
+        borderRadius: 10,
+        paddingHorizontal: 20,
+        paddingVertical: 10
+    },
+    textTitleCategory: {
+        fontSize: 16,
+        color: Constant.color.text,
+        fontFamily: Constant.fonts.robotoSlabSemiBold
     }
 })
